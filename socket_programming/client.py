c146e1761e30fb13197fd4286699a792a2555fa6
Alief Gilang <64684740+lyfesan@users.noreply.github.com>
2024-10-27 20:11:24 +0700

    fix: Add raw data to output

--- socket_programming/client.py
+++ socket_programming/client.py
@@ -13,18 +13,22 @@ def client_program():
     host = socket.gethostname()
     port = 9999
 
+    # Open connection to server
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))  # connect to the server
+    client_socket.connect((host, port))
 
     message = input(" -> ") 
 
     while message.lower().strip() != 'exit':
+        # Encrypt message
         message = DES.batch_encrypt(message, key)
-        client_socket.send(message.encode())  # send message
+        client_socket.send(message.encode())
         
-        data = client_socket.recv(1024).decode()
-        data = DES.batch_decrypt(data, key)
+        # Receive and decrypt data 
+        data_raw = str(client_socket.recv(1024).decode())
+        data = DES.batch_decrypt(data_raw, key)
         
+        print('Raw data from server: ' + data_raw)
         print('Received from server: ' + data)
 
         message = input(" -> ")
--- socket_programming/server.py
+++ socket_programming/server.py
@@ -9,30 +9,32 @@ sys.path.insert(0, parent_dir)
 from DES import DES
 
 def server_program():
-    key = 'AkuKamus'
-    # get the hostname
+    key = 'AkuKamus' # key used for DES
     host = socket.gethostname()
-    port = 9999  # initiate port no above 1024
+    port = 9999 
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
+    # Create socket connection
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
 
-    # configure how many client the server can listen simultaneously
     server_socket.listen(2)
     conn, address = server_socket.accept()  # accept new connection
     print("Connection from: " + str(address))
     while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = DES.batch_decrypt(str(conn.recv(1024).decode()), key)
+        # receive data stream and decrypt it
+        data_raw = str(conn.recv(1024).decode())
+        data = DES.batch_decrypt(data_raw, key)
         
+        print("Raw data from connected user: " + data_raw)
         print("from connected user: " + data)
+        
+        # Send data to client
         data = input(' -> ')
+        # Encrypt data
         data = DES.batch_encrypt(data, key)
-        conn.send(data.encode())  # send data to the client
+        conn.send(data.encode())
 
-    conn.close()  # close the connection
-
+    conn.close()
 
 if __name__ == '__main__':
     server_program()
